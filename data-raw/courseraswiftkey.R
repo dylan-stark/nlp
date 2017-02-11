817d248d82d46a04053e919770b0fa3fc1d4a30a
Dylan Stark <dylan.stark@gmail.com>
2017-02-11 13:59:50 -0600

    Adds correlation measurement.

--- data-raw/courseraswiftkey.R
+++ data-raw/courseraswiftkey.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 library(stringr)
-library(tidytext)
 
 set.seed(42)
 
@@ -36,9 +35,9 @@ if(!file.exists("data-raw/final")) {
 # Sample ~10,000 records from each file
 text <- tribble(
   ~filename, ~size,
-  "data-raw/final/en_US/en_US.blogs.txt", 0.01111991,
-  "data-raw/final/en_US/en_US.news.txt", 0.009898618,
-  "data-raw/final/en_US/en_US.twitter.txt", 0.004237022
+  "data-raw/final/en_US/en_US.blogs.txt", 0.01,
+  "data-raw/final/en_US/en_US.news.txt", 0.01,
+  "data-raw/final/en_US/en_US.twitter.txt", 0.01
 ) %>%
   mutate(raw_text = map2(filename, size, text_sample_frac))
 
